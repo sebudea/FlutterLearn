cff240a6d1e59216be9848dff1ca8555dd1dfc52
Sebastian Aristizabal Cast <sebastian.aristizabalc@udea.edu.co>
2023-12-13 19:02:45 -0500

    Flutter iconos

--- lib/main.dart
+++ lib/main.dart
@@ -14,13 +14,13 @@ class MyApp extends StatelessWidget {
           title: const Text('Material App Bar'),
         ),
         body: Center(
-          child: Image.network(
-            "https://i.pinimg.com/originals/05/3c/99/053c99184587c3264c91976f184c49e3.gif",
-            height: 200,
-            width: 200,
-            fit: BoxFit.cover,
-          ),
-        ),
+            child: IconButton(
+          onPressed: () {
+            print("Hola");
+          },
+          icon: Icon(Icons.close),
+          iconSize: 200,
+        )),
       ),
     );
   }
