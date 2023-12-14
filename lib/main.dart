721d112467504068cb6e972a8045f7f6ade19a7d
Sebastian Aristizabal Cast <sebastian.aristizabalc@udea.edu.co>
2023-12-14 17:59:53 -0500

    Flutter StatefullWidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
@@ -8,16 +10,79 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(fontFamily: "GloriaHallelujah"),
       title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String name = "Marvin";
+  double progressValue = 0.0;
+  bool switchValue = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Column(
+          children: [
+            Text(
+              name,
+              style: TextStyle(fontSize: 30),
+            ),
+            LinearProgressIndicator(
+              value: progressValue,
+            ),
+            Switch(
+              value: switchValue,
+              onChanged: (value) {
+                setState(() {
+                  switchValue = value;
+                });
+              },
+            )
+          ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.refresh),
+        onPressed: changeName,
+      ),
     );
+  }
+
+  void changeName() {
+    setState(() {
+      if (name == "Marvin") {
+        name = "Osiel";
+      } else {
+        name = "Marvin";
+      }
+      progressValue += 0.01;
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Timer.periodic(Duration(seconds: 1), (value) {
+      changeName();
+    });
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
   }
 }
